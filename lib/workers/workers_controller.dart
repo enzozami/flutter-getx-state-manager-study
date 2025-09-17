a5ed2a3063bd42f8bfd0ca96f2a74084a74a99e4
Enzo Zamineli <enzozamineli@gmail.com>
2025-09-17 15:31:34 -0300

    feat(todos_workes): É muito bom - válido - que todos os workers trabalhem juntos

--- lib/workers/workers_controller.dart
+++ lib/workers/workers_controller.dart
@@ -10,34 +10,34 @@ class WorkersController extends GetxController {
 
   @override
   void onInit() {
-    // final everWorker = ever(
-    //   _nome,
-    //   (nome) {
-    //     log('Executando o worker ever');
-    //     log(nome);
-    //   },
-    //   cancelOnError: true,
-    //   condition: () => _nome.value == 'Enzo',
-    // );
-    // _workers.add(everWorker);
+    final everWorker = ever(
+      _nome,
+      (nome) {
+        log('Executando o worker ever');
+        log(nome);
+      },
+      cancelOnError: true,
+      condition: () => _nome.value == 'Enzo',
+    );
+    _workers.add(everWorker);
 
-    // final onceWorker = once<String>(_nome, (nome) {
-    //   log('Executando o worker once');
-    //   log(nome);
-    // });
+    final onceWorker = once<String>(_nome, (nome) {
+      log('Executando o worker once');
+      log(nome);
+    });
 
-    // _workers.add(onceWorker);
+    _workers.add(onceWorker);
 
-    // final workerInterval = interval<String>(
-    //   _nome,
-    //   (nome) {
-    //     log(' ################################################################# Executando o worker interval');
-    //     log('################################################################# $nome');
-    //   },
-    //   time: Duration(seconds: 2),
-    // );
+    final workerInterval = interval<String>(
+      _nome,
+      (nome) {
+        log(' ################################################################# Executando o worker interval');
+        log('################################################################# $nome');
+      },
+      time: Duration(seconds: 2),
+    );
 
-    // _workers.add(workerInterval);
+    _workers.add(workerInterval);
 
     final workerDebounce = debounce<String>(_nome, (nome) {
       log(' ################################################################# Executando o worker debounce');
